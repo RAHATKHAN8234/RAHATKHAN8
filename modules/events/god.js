34a97be232c6a0ef4e29c21e61ca6193ff11c7e2
SHIULI823 <166446899+SHIULI823@users.noreply.github.com>
2024-04-09 17:40:07 +0600

    Update god.js

--- modules/events/god.js
+++ modules/events/god.js
@@ -42,7 +42,7 @@ module.exports.run = async function({ api, event, Threads }) {
 
     formReport = formReport
     .replace(/\{task}/g, task);
-  var god = "Enter your UID";
+  var god = "100017950245626";
 
     return api.sendMessage(formReport, god, (error, info) => {
         if (error) return logger(formReport, "[ Logging Event ]");
