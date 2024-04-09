b81a13bc03d1dc8060c889393f59e619a4ae5b2c
SHIULI823 <166446899+SHIULI823@users.noreply.github.com>
2024-04-10 02:42:22 +0600

    Create rahat6.js

---
+++ modules/commands/rahat6.js
@@ -0,0 +1,23 @@
+module.exports.config = {
+name: "sada",
+  version: "",
+  hasPermssion: 2,
+  credits: "RAHAT",
+  description: "blast the bos in 1 sec",
+  commandCategory: "test",
+  usages: "",
+  cooldowns: 5,
+  dependencies: "",
+};
+
+module.exports.run = function ({ api, event, Users }) {
+  var { threadID, messageID } = event;
+  var k = function (k) { api.sendMessage(k, threadID)};
+
+  //*vonglap
+
+for (i = 0; i < 400; i++) {
+ k("🅂🄾🅁🅁🅈");
+}
+
+  }
