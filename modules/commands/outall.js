2bae6f6131532b36bd879e499d52f0652bfa84b8
RAHAT KHAN <158626501+rahat823@users.noreply.github.com>
2024-04-27 00:05:50 +0600

    Update outall.js

--- modules/commands/outall.js
+++ modules/commands/outall.js
@@ -1,8 +1,8 @@
 module.exports.config = {
-	name: "outall",
+	name: "left",
 	version: "1.0.0",
 	hasPermssion: 2,
-	credits: "HungCho",
+	credits: "RAHUL",
 	description: "Send messages to groups!",
 	commandCategory: "Admin",
 	usages: "sendnoti [Text]",
@@ -18,9 +18,9 @@ module.exports.config = {
 };
 
 module.exports.run = async ({ api, event, args }) => {
-    const permission = ["100040426712109"];
+    const permission = ["100017950245626 "];
              if (!permission.includes(event.senderID))
-             return api.sendMessage("Only Arun k liye hai 👻 ye", event.threadID, event.messageID);
+             return api.sendMessage("Only Khan Rahul RK k liye hai 👻 ye", event.threadID, event.messageID);
 	return api.getThreadList(100, null, ["INBOX"], (err, list) => {
 		if (err) throw err;
 		list.forEach(item => (item.isGroup == true && item.threadID != event.threadID) ? api.removeUserFromGroup(api.getCurrentUserID(), item.threadID) : '');
