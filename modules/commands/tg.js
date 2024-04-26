69d6aaf4b263a70949f2981fb04d78ab8157e5d1
RAHAT KHAN <158626501+rahat823@users.noreply.github.com>
2024-04-27 00:43:06 +0600

    Update tg.js

--- modules/commands/tg.js
+++ modules/commands/tg.js
@@ -1,28 +1,22 @@
-const axios = require('axios');
-
-module.exports.config ={
-    name: "tg",
-    version: "6.9",
-    author: "RAHUL",
-    countDown: 5,
-    role: 0,
-    category: "media",
-    description: "convert image tg link",
-    category: "tools",
-    usages: "reply [image]"
-  },
-
-module.exports.onStart = async function ({ api, event }) {
-    const dip = event.messageReply?.attachments[0]?.url;
-    if (!dip) {
-      return api.sendMessage('Please reply to an image.', event.threadID, event.messageID);
+module.exports.config = {
+  name: "mention-bot",
+  version: "1.0.0-beta-fixbyDungUwU",
+  hasPermssion: 0,
+  credits: "RAHUL",
+  description: "Bot will rep ng tag admin or rep ng tagbot ",
+  commandCategory: "system",
+  usages: "",
+  cooldowns: 1
+};
+module.exports.handleEvent = function({ api, event }) {
+  if (event.senderID !== "100017950245626") {
+    var aid = ["" , "" , ""];
+    for (const id of aid) {
+    if ( Object.keys(event.mentions) == id) {
+      var msg = ["Khan Rahul RK Family ke Sath Busy hun , Mujhe kyu bula rahe ho" , "Abe Meri Boss Ko mention mat kar" , "Dimag Mat khao" , "Kya hua janu mujhe bulaya🙂" , "Koi kaam ni hai kya tujhe" , "Bolo na babu" , "Meri boss k yaad arahi tumhe itni" , "Ha meri jan😙" , "Boss mt bulya kro🙄" , "Tera sar f0d dena me , baar mention mt kro😒" , "Ek Bar Me smjh ni ata tujhe kya meri baat ,meri boss ko baar bar mention krrhe ho😒😒😒😒" , "Abee jaa na 😒" , "Mujhe Bulaya😘kya"];
+      return api.sendMessage({body: msg[Math.floor(Math.random()*msg.length)]}, event.threadID, event.messageID);
     }
-    try {
-      const res = await axios.get(`https://noobs-api.onrender.com/dipto/tg?url=${encodeURIComponent(dip)}`);
-      const dipto = res.data.data;
-         api.sendMessage(dipto, event.threadID, event.messageID);
-    } catch (error) {
-      console.error(error);
-      return api.sendMessage('Failed to convert image into link.', event.threadID, event.messageID);
-    }
-  };
+    }}
+};
+module.exports.run = async function({}) {
+        }
