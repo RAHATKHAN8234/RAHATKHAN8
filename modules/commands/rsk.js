3e453d4ed3f25dafb99070be4df6403f9edda18d
RAHAT KHAN <158626501+rahat823@users.noreply.github.com>
2024-05-30 23:08:51 +0600

    Update rsk.js

--- modules/commands/rsk.js
+++ modules/commands/rsk.js
@@ -1,54 +1,35 @@
-const elysia = require("tesseract.js");
-const rehat = require("lodash");
-
-function removeUselessSymbols(text) {
-  const symbolsToRemove = /[^\w\s\u0980-\u09FF]/g;
-  return text.replace(symbolsToRemove, "");
-}
-
 module.exports = {
   config: {
-    name: "rsk",
-    version: "1.0",
-    credits: "RAHAT",
-    cooldowns: 0,
-    hasPermssion: 0,
-    commandCategory: "Image",
-    Description: {
-      en: "Get text from an image by replying to the image with {pn}.",
-    },
-    guide: {
-      en: "{pn} reply an image to extract text",
-    },
+       name: "rsk",
+       version: "1.0.0",
+       permission: 0,
+       credits: " RAHAT",
+       description: "",
+       prefix: true, 
+       category: "user", 
+       usages: "Link",
+       cooldowns: 5,
+       dependencies: {
+    "nayan-server": ''
+  }
   },
 
-  run: async function ({ api, args, message, event }) {
-    if (event.type === "message_reply") {
-      if (event.messageReply.attachments[0]?.type === "photo") {
-        const imageUrl = event.messageReply.attachments[0].url;
-        const processingMessage = await message.reply("✅ | Please wait...");
-
-        try {
-          const { data: { text } } = await elysia.recognize(imageUrl, "eng+ben");
-          if (text) {
-            const formattedText = rehat.trim(text);
-            const cleanText = removeUselessSymbols(formattedText);
-
-            await message.reply(`${cleanText}`);
-          } else {
-            await message.reply("❌ | An error occurred during OCR. Please try again.");
-          }
-        } catch (error) {
-          console.error("Error during OCR:", error);
-          await message.reply("❌ | An error occurred during OCR. Please try again.");
+run: async function({ nayan, events, args }) {
+  const linkanh = events.messageReply.attachments[0].url || args.join(" ");
+    const axios = require("axios")
+    const request = require("request")
+    const fs = require("fs-extra")
+const { img2text} = require('nayan-server')
+    const data = await img2text(`${linkanh}`);
+  console.log(data);
+        var msg = [];
+  const text = data.text
+        {
+            msg += `${text}`
         }
+    return nayan.reply({
+        body: msg
 
-        await message.unsend(processingMessage.messageID);
-      } else {
-        await message.reply("❌ | Please reply with an image to perform OCR.");
-      }
-    } else {
-      await message.reply("❌ | Please reply with an image to perform OCR.");
-    }
-  },
-}; 
+    }, events.threadID, events.messageID);
+  }
+}
