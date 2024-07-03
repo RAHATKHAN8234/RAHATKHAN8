0cb1bbb0423a1cea36f141a6dd6a6769d0f95be5
RAHAT KHAN <158626501+rahat823@users.noreply.github.com>
2024-07-03 22:52:51 +0600

    Update dalle.js

--- modules/commands/dalle.js
+++ modules/commands/dalle.js
@@ -1,48 +1,72 @@
-const axios = require('axios');
-const fs = require('fs-extra');
+const axios = require("axios");
+const fs = require("fs-extra");
+const baseApiUrl = async () => {
+  const base = await axios.get(
+    `https://raw.githubusercontent.com/Blankid018/D1PT0/main/baseApiUrl.json`,
+  );
+  return base.data.api;
+};
+
 module.exports.config = {
-    name: "dalle",
-    version: "1.0",
-    credits: "RAHUL",
-    hasPermssion: 0,
-    usePrefix: true,
-    description: "Generate images by Dalle-3 AI",
-    commandCategory: "download",
-    usages: "[text] \nJamon [A 17/18/19 years old boy/girl watching football match on tv and written RAHUL and 69 on the back of his Dress , 4k]",
-    cooldowns: 5
-  };
+  name: "dalle",
+  version: "1.0",
+  credits: "RAHUL",
+  hasPermssion: 0,
+  usePrefix: true,
+  description: "Generate images by Dalle-3 AI",
+  commandCategory: "download",
+  usages:
+    "[text] \nJamon [A 17/18/19 years old boy/girl watching football match on tv and written RAHUL and 69 on the back of his Dress , 4k]",
+  cooldowns: 5,
+};
 
 module.exports.run = async function ({ api, event, args }) {
-  const prompt = event.messageReply?.body.split("dalle")[1] ||  args.join(" ");
-  if (!prompt) {
-   return api.sendMessage("❌| use /dalle a cat , 4k",event.threadID,event.messageID);
-  }
-    try {
-      //const cookies = "cookies dew";
-const tl = ["1rE4RtLEt8-oQ0oNO0q__89GSkcyj1nVOaOghemAYShfgLhVIBN-w7viryjQVlIM2femcgs-Fh7nATVu7ndudbffL4ziJ9qh-WiIuChyHkBBHmrdQ22Xtz-geIt18y-gbemm4uemaDS0UH3CFQF3g1-GA1NyGPIb3jaWLUDCW9AdhT97Ekt4qYTna429pLZP8eAOLLOeW66OSd2igtbpCuw"];
-const cookies = tl[Math.floor(Math.random() * tl.length)];
-      const w = await api.sendMessage("𝐏𝐥𝐞𝐚𝐬𝐞 𝐖𝐚𝐢𝐭 𝐏𝐫𝐨𝐜𝐞𝐬𝐬𝐢𝐧𝐠 𝐘𝐨𝐮𝐫 𝐈𝐦𝐠𝐚𝐞\n\n𝐂𝐫𝐞𝐚𝐭𝐞 𝐁𝐲 𝐊𝐡𝐚𝐧 𝐑𝐚𝐡𝐮𝐥 𝐑𝐊", event.threadID);
-  
-const response = await axios.get(`https://nobs-api.onrender.com/dipto/dalle?prompt=${prompt}&key=dipto008&cookies=${cookies}`)
-      const data = response.data.imgUrls;
-      if (!data || data.length === 0) {
-        api.sendMessage("No images generated.",event.threadID,event.messageID);
-      }
-      const diptoo = [];
-      for (let i = 0; i < data.length; i++) {
-        const imgUrl = data[i];
-        const imgResponse = await axios.get(imgUrl, { responseType: 'arraybuffer' });
-        const imgPath = __dirname + `/cache/${i + 1}.jpg`;
-        await fs.outputFile(imgPath, imgResponse.data);
-        diptoo.push(fs.createReadStream(imgPath));
-      }
-      await api.unsendMessage(w.messageID);
-      await api.sendMessage({
-  body: `𝐒𝐮𝐜𝐜𝐞𝐬𝐬𝐟𝐮𝐥 𝐆𝐞𝐧𝐞𝐫𝐚𝐭𝐞 𝐘𝐨𝐮𝐫 𝐈𝐦𝐚𝐠𝐞\n\n𝐂𝐫𝐞𝐚𝐭𝐞 𝐁𝐲 𝐊𝐡𝐚𝐧 𝐑𝐚𝐡𝐮𝐥 𝐑𝐊`,
-        attachment: diptoo
-      },event.threadID, event.messageID);
-    } catch (error) {
-      console.error(error);
-      await api.sendMessage(`Generation failed!\nError: ${error.message}`,event.threadID, event.messageID);
-    }
-  }; 
+  const prompt = event.messageReply?.body.split("dalle")[1] || args.join(" ");
+  if (!prompt) {
+    return api.sendMessage(
+      "❌| Wrong Formet .✅ | Use 17/18 years old boy/girl watching football match on tv and written Dipto and 69 on the back of his Dress , 4k",
+      event.threadID,
+      event.messageID,
+    );
+  }
+  try {
+    //const cookies = "cookies here (_U value)";
+    const tl = ["1rE4RtLEt8-oQ0oNO0q__89GSkcyj1nVOaOghemAYShfgLhVIBN-w7viryjQVlIM2femcgs-Fh7nATVu7ndudbffL4ziJ9qh-WiIuChyHkBBHmrdQ22Xtz-geIt18y-gbemm4uemaDS0UH3CFQF3g1-GA1NyGPIb3jaWLUDCW9AdhT97Ekt4qYTna429pLZP8eAOLLOeW66OSd2igtbpCuw",];
+    const cookies = tl[Math.floor(Math.random() * tl.length)];
+    const w = await api.sendMessage("𝙥𝙡𝙚𝙖𝙨𝙚 𝙬𝙞𝙩𝙝𝙚 𝙥𝙧𝙤𝙘𝙚𝙨𝙨𝙞𝙣𝙜 𝙮𝙤𝙪𝙧 𝙞𝙢𝙖𝙜𝙚 \n𝙠𝙝𝙖𝙣 𝙧𝙖𝙝𝙪𝙡 𝙧𝙠💞", event.threadID);
+
+    const response = await axios.get(
+      `${await baseApiUrl()}/dalle?prompt=${prompt}&key=dipto008&cookie=${cookies}`,
+    );
+    const data = response.data.imgUrls;
+    if (!data || data.length === 0) {
+      api.sendMessage("No images generated.", event.threadID, event.messageID);
+    }
+    const diptoo = [];
+    for (let i = 0; i < data.length; i++) {
+      const imgUrl = data[i];
+      const imgResponse = await axios.get(imgUrl, {
+        responseType: "arraybuffer",
+      });
+      const imgPath = __dirname + `/cache/${i + 1}.jpg`;
+      await fs.outputFile(imgPath, imgResponse.data);
+      diptoo.push(fs.createReadStream(imgPath));
+    }
+    await api.unsendMessage(w.messageID);
+    await api.sendMessage(
+      {
+        body: `𝙨𝙪𝙘𝙘𝙚𝙨𝙨 𝙨𝙪𝙘𝙘𝙚𝙨𝙨𝙛𝙪𝙡 𝙮𝙤𝙪𝙧 𝙞𝙢𝙖𝙜𝙚 \n𝙠𝙝𝙖𝙣 𝙧𝙖𝙝𝙪𝙡 𝙧𝙠💞`,
+        attachment: diptoo,
+      },
+      event.threadID,
+      event.messageID,
+    );
+  } catch (error) {
+    console.error(error);
+    await api.sendMessage(
+      `Generation failed!\nError: ${error.message}`,
+      event.threadID,
+      event.messageID,
+    );
+  }
+};
