38240f57c1b8798d65886ec7d5f6ae20227020e5
RAHATKHAN8234 <rifatrahatkhan@gmail.com>
2024-07-16 00:04:10 +0600

    Update dallev3.js

--- modules/commands/dallev3.js
+++ modules/commands/dallev3.js
@@ -1,48 +1,74 @@
-const axios = require('axios');
-const fs = require('fs-extra');
+const axios = require("axios");
+const fs = require("fs-extra");
+const baseApiUrl = async () => {
+  const base = await axios.get(
+    `https://raw.githubusercontent.com/Blankid018/D1PT0/main/baseApiUrl.json`,
+  );
+  return base.data.api;
+};
+
 module.exports.config = {
-    name: "create",
-    version: "1.0",
-    credits: "RAHUL",
-    hasPermssion: 0,
-    usePrefix: true,
-    description: "Generate images by Dalle-3 AI",
-    commandCategory: "download",
-    usages: "[text] \nJamon [A 17/18/19 years old boy/girl watching football match on tv and written RAHUL and 69 on the back of his Dress , 4k]",
-    cooldowns: 5
-  };
+  name: "create",
+  version: "1.0",
+  credits: "𝐑𝐀𝐇𝐀𝐓",
+  hasPermssion: 0,
+  usePrefix: true,
+  prefix: true,
+  description: "Generate images by Dalle-3 AI",
+  commandCategory: "download",
+  category: "download",
+  usages:
+    "[text] \nJamon [A 17/18/19 years old boy/girl watching football match on tv and written RAHAT and 69 on the back of his Dress , 4k]",
+  cooldowns: 5,
+};
 
 module.exports.run = async function ({ api, event, args }) {
-  const prompt = event.messageReply?.body.split("dalle")[1] ||  args.join(" ");
-  if (!prompt) {
-   return api.sendMessage("❌| use /dalle a cat , 4k",event.threadID,event.messageID);
-  }
-    try {
-      //const cookies = "your cookies";
-const tl = ["15rhShQp9j4dQdSZm8LY2e8AjPjGZgEf4NxxOCF7qKU_pG57nHpc9CulkUigL_GSY8i5UoutzVelp4yRTJJfF4C3Al85RuLTTi7ESi8d-7lHCow4YeT-9jVrp6cq_OcEmB6MklD-naUpKm0YICqVAWjgRpA9fb6X9ybGTRxEX_rBEXAVlP9ZpBX_K-YEyZmAzJteTdCxQUjhNnnetFmxN_Q"];
-const cookies = tl[Math.floor(Math.random() * tl.length)];
-      const w = await api.sendMessage("একটু সময় অপেক্ষা করো কলিজা😍", event.threadID);
-  
-const response = await axios.get(`https://raw.githubusercontent.com/Blankid018/D1PT0/main/baseApiUrl.json/dalle?prompt=${prompt}&key=dipto008&cookies=${cookies}`)
-      const data = response.data.imgUrls;
-      if (!data || data.length === 0) {
-        api.sendMessage("No images generated.",event.threadID,event.messageID);
-      }
-      const diptoo = [];
-      for (let i = 0; i < data.length; i++) {
-        const imgUrl = data[i];
-        const imgResponse = await axios.get(imgUrl, { responseType: 'arraybuffer' });
-        const imgPath = __dirname + `/cache/${i + 1}.jpg`;
-        await fs.outputFile(imgPath, imgResponse.data);
-        diptoo.push(fs.createReadStream(imgPath));
-      }
-      await api.unsendMessage(w.messageID);
-      await api.sendMessage({
-  body: `এই নাও তোমার ইমেজ কলিজা😘`,
-        attachment: diptoo
-      },event.threadID, event.messageID);
-    } catch (error) {
-      console.error(error);
-      await api.sendMessage(`Generation failed!\nError: ${error.message}`,event.threadID, event.messageID);
-    }
-  }; 
+  const prompt = event.messageReply?.body.split("dalle")[1] || args.join(" ");
+  if (!prompt) {
+    return api.sendMessage(
+      "❌| Wrong Formet .✅ | Use 17/18 years old boy/girl watching football match on tv and written RAHAT and 69 on the back of his Dress , 4k",
+      event.threadID,
+      event.messageID,
+    );
+  }
+  try {
+    //const cookies = "cookies here (_U value)";
+    const tl = ["1_fIyvMBd4AdvnAatXwtpJ6xK5wlhcmUdP_gDCBwjMkBhVHTm3dsRCz6fHkUDoWn3vycxnLsoo6NsnzWR1hawF78Ldncg-ACPgwe-ypoRfT1HYcdby-7DhG3XorB6O2WRCNxoimpUteOHvsJ-a0d3xGWIXw825Og4MG0AEiVB3ykn6GEeq4U__GnaqW4ih9qdOcU_-h2DPRmLCcj_0faO9Q", "1_fIyvMBd4AdvnAatXwtpJ6xK5wlhcmUdP_gDCBwjMkBhVHTm3dsRCz6fHkUDoWn3vycxnLsoo6NsnzWR1hawF78Ldncg-ACPgwe-ypoRfT1HYcdby-7DhG3XorB6O2WRCNxoimpUteOHvsJ-a0d3xGWIXw825Og4MG0AEiVB3ykn6GEeq4U__GnaqW4ih9qdOcU_-h2DPRmLCcj_0faO9Q"];
+    const cookies = tl[Math.floor(Math.random() * tl.length)];
+    const w = await api.sendMessage("𝐖𝐚𝐢𝐭 𝐤𝐨𝐫𝐨 𝐛𝐚𝐛𝐲 < \n\n𝐀𝐩𝐢 𝐊𝐡𝐚𝐧 𝐑𝐚𝐡𝐮𝐥 𝐑𝐊😘", event.threadID);
+
+    const response = await axios.get(
+      `${await baseApiUrl()}/dalle?prompt=${prompt}&key=dipto008&cookie=${cookies}`,
+    );
+    const data = response.data.imgUrls;
+    if (!data || data.length === 0) {
+      api.sendMessage("No images generated.", event.threadID, event.messageID);
+    }
+    const diptoo = [];
+    for (let i = 0; i < data.length; i++) {
+      const imgUrl = data[i];
+      const imgResponse = await axios.get(imgUrl, {
+        responseType: "arraybuffer",
+      });
+      const imgPath = __dirname + `/cache/${i + 1}.jpg`;
+      await fs.outputFile(imgPath, imgResponse.data);
+      diptoo.push(fs.createReadStream(imgPath));
+    }
+    await api.unsendMessage(w.messageID);
+    await api.sendMessage(
+      {
+        body: `✅𝐍𝐚𝐰 𝐁𝐚𝐛𝐲 𝐓𝐮𝐦𝐚𝐫 𝐆𝐞𝐧𝐞𝐫𝐚𝐭𝐞d 𝐏𝐢𝐜<😘 \n\n𝐀𝐩𝐢 𝐊𝐡𝐚𝐧 𝐑𝐚𝐡𝐮𝐥 𝐑𝐊`,
+        attachment: diptoo,
+      },
+      event.threadID,
+      event.messageID,
+    );
+  } catch (error) {
+    console.error(error);
+    await api.sendMessage(
+      `Generation failed!\nError: ${error.message}`,
+      event.threadID,
+      event.messageID,
+    );
+  }
+};
