a98d5f23d2edaac07a45c202a30179e06257ae86
RAHATKHAN8234 <rifatrahatkhan@gmail.com>
2024-07-16 15:00:43 +0600

    Update ajan.js

--- modules/commands/ajan.js
+++ modules/commands/ajan.js
@@ -1,52 +1,61 @@
 module.exports.config = {
   name: "ajan",
   version: "2.0",
-  hasPermission: 0,
-  credits: "RAHAT",
+  role: 0,
+  author: "𝐊𝐡𝐚𝐧 𝐑𝐚𝐡𝐮𝐥 𝐑𝐊",
   description: "সেট করা সময় অনুযায়ী স্বয়ংক্রিয়ভাবে বার্তাগুলি পাঠানো হবে!",
-  usePrefix: true,
-  commandCategory: "AutoTime",
-  cooldowns: 3,
+  category: "AutoTime",
+  countDown: 3,
 };
 
 module.exports.onLoad = async ({ api }) => {
   const timerData = {
-      "03:55:00 AM": {
-        message: "Fajr Prayers Alert",
-        url: "https://i.imgur.com/C0bFtoh.jpeg"
-      },
-      "12:45:00 PM": {
-        message: "Dhuhr Prayers Alert",
-        url: "https://i.imgur.com/vtOBIkA.jpeg"
-      },
-      "04:45:00 PM": {
-        message: "Asr Prayers Alert",
-        url: "https://i.imgur.com/hprI30O.jpeg"
-      },
-      "06:48:00 PM": {
-        message: "Maghrib Prayers Alert",
-        url: "https://i.imgur.com/vlFSFL6.jpeg"
-      },
-      "08:15:00 PM": {
-        message: "Isha Prayers Alert",
-        url: "https://i.imgur.com/7K67WOG.jpeg"
-      }
+    "04:15 AM": {
+      message: "এখন ফরজ এর আজান দিবে সবাই নামাজ পরে নাও",
+      url: "https://i.imgur.com/7K67WOG.jpeg"
+    },
+    "12:45:00 PM": {
+      message: "এখন যোহর এর আজান দিবে সবাই নামাজ পড়েন",
+      url: "https://i.imgur.com/vtOBIkA.jpeg"
+    },
+    "04:45:00 PM": {
+      message: "এখন আছরের আজান দিবে সবাই নামাজ আদায় করেন",
+      url: "https://i.imgur.com/hprI30O.jpeg"
+    },
+    "06:45:00 PM": {
+      message: "এখন মাগরিব এর আজান হবে সবাই নামাজ আদায় করবেন",
+      url: "https://i.imgur.com/vlFSFL6.jpeg"
+    },
+    "08:00:00 PM": {
+      message: "এখন এশার আজান দিবে এই সব কাজ অফ করে নামাজ পড়েন",
+      url: "https://i.imgur.com/7K67WOG.jpeg"
+    }
   };
-  if(timerData){
-const checkTimeAndSendMessage = async() => { 
-  const currentTime = new Date(Date.now() + 21600000).toLocaleTimeString('en-US', {
-    hour: '2-digit',
-    minute: '2-digit',
-    second: '2-digit',
-    hour12: true
-  }).split(',').pop().trim(); 
-  const attachment = (await require('axios').get(timerData[currentTime].url, { responseType: 'stream' })).data;
 
-    if (timerData[currentTime]) global.data.allThreadIDs.forEach(async threadID => await api.sendMessage({body: timerData[currentTime].message, attachment }, threadID)); 
-    setTimeout(checkTimeAndSendMessage, 1200 - new Date().getMilliseconds()); 
-   }; 
+  const checkTimeAndSendMessage = async () => {
+    const currentTime = new Date(Date.now() + 21600000).toLocaleTimeString('en-US', {
+      hour: '2-digit',
+      minute: '2-digit',
+      hour12: true
+    }).split(',').pop().trim();
+
+    if (timerData[currentTime]) {
+      console.log(timerData[currentTime].message);
+      console.log(timerData[currentTime].url);
+      try {
+       let messageData = { body: timerData[currentTime].message,attachment:(await require('axios').get(timerData[currentTime].url, { responseType: 'stream' })).data };
+
+        global.data.allThreadID.forEach(async threadID => {
+          await api.sendMessage(messageData, threadID);
+        });
+      } catch (error) {
+        console.error(`Failed to send message for time ${currentTime}:`, error);
+      }
+    }
+    setTimeout(checkTimeAndSendMessage, 45000);
+  };
+
   checkTimeAndSendMessage();
- }
 };
 
-module.exports.run = ({}) => {};
+module.exports.run= ({}) => {};
