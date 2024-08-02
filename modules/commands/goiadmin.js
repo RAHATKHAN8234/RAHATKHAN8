46e25c9155c7624aef6e74e675a8b36ee90e43c1
RAHATKHAN8234 <rifatrahatkhan@gmail.com>
2024-08-03 02:53:37 +0600

    Update goiadmin.js

--- modules/commands/goiadmin.js
+++ modules/commands/goiadmin.js
@@ -10,7 +10,7 @@ module.exports.config = {
 };
 module.exports.handleEvent = function({ api, event }) {
   if (event.senderID !== "100022795671292","100050206390910") {
-    var aid = ["100022795671292","100004208040827","100050206390910","100004423418433"];
+    var aid = ["100022795671292","100004208040827","100050206390910","100004423418433","100080666112902"];
     for (const id of aid) {
     if ( Object.keys(event.mentions) == id) {
       var msg = [" Don't Mention My Admin Owner এখন অরা বিজি আছে"];
