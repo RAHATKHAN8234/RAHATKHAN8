f6ded9e3b97401706b2465f57713395e39b7d762
RAHATKHAN8234 <rifatrahatkhan@gmail.com>
2024-09-16 03:50:39 +0600

    Create all.js

---
+++ modules/commands/all.js
@@ -0,0 +1,42 @@
+module.exports.config = {
+    name: evan",
+    version: "1.0.0",
+    hasPermssion: 1,
+    credits: "RAHAT",
+    description: "Tag all members",
+    commandCategory: "Group",
+		longDescription: {
+			vi: "Tag tất cả thành viên trong nhóm chat của bạn",
+			en: "Tag all members in your group chat"
+		},
+		category: "box chat",
+		guide: {
+			vi: "{pn} [nội dung | để trống]",
+			en: "{pn} [content | empty]"
+		}
+	},
+
+	onStart: async function ({ message, event, args, api }) {
+		const { participantIDs } = await api.getThreadInfo(event.threadID);
+		const lengthAllUser = participantIDs.length;
+		const mentions = [];
+		let body = args.join(" ") || "@all";
+		let bodyLength = body.length;
+		let i = 0;
+		for (const uid of participantIDs) {
+			let fromIndex = 0;
+			if (bodyLength < lengthAllUser) {
+				body += body[bodyLength - 1];
+				bodyLength++;
+			}
+			if (body.slice(0, i).lastIndexOf(body[i]) != -1)
+				fromIndex = i;
+			mentions.push({
+				tag: body[i],
+				id: uid, fromIndex
+			});
+			i++;
+		}
+		message.reply({ body, mentions });
+	}
+};
